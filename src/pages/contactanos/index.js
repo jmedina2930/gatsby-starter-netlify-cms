c9f6952db29c28c3c13eab1abf7a66251ab6c8cb
Andres0103 <austindiaz963@gmail.com>
2020-03-13 15:57:18 -0500

    cambios en textos y agregue campos

--- src/pages/contactanos/index.js
+++ src/pages/contactanos/index.js
@@ -39,7 +39,7 @@ export default class Index extends React.Component {
         <section className="section">
           <div className="container">
             <div className="content">
-              <h1>Contact</h1>
+              <h1>Contáctenos</h1>
               <form
                 name="contact"
                 method="post"
@@ -58,7 +58,7 @@ export default class Index extends React.Component {
                 </div>
                 <div className="field">
                   <label className="label" htmlFor={"name"}>
-                    Your name
+                    Nombre
                   </label>
                   <div className="control">
                     <input
@@ -73,7 +73,7 @@ export default class Index extends React.Component {
                 </div>
                 <div className="field">
                   <label className="label" htmlFor={"email"}>
-                    Email
+                    Correo
                   </label>
                   <div className="control">
                     <input
@@ -88,7 +88,7 @@ export default class Index extends React.Component {
                 </div>
                 <div className="field">
                   <label className="label" htmlFor={"message"}>
-                    Message
+                    Mensaje
                   </label>
                   <div className="control">
                     <textarea
@@ -96,6 +96,36 @@ export default class Index extends React.Component {
                       name={"message"}
                       onChange={this.handleChange}
                       id={"message"}
+                      required={true}
+                    />
+                  </div>
+                </div>
+                  <div className="field">
+                  <label className="label" htmlFor={"empresa"}>
+                    Empresa
+                  </label>
+                  <div className="control">
+                    <input
+                      className="input"
+                      type={"empresa"}
+                      name={"username"} 
+                      onChange={this.handleChange}
+                      id={"username"}
+                      required={false}
+                    />
+                  </div>
+                </div>
+                <div className="field">
+                  <label className="label" htmlFor={"telefono"}>
+                    Teléfono
+                  </label>
+                  <div className="control">
+                    <input
+                      className="input"
+                      type={"text"}
+                      name={"telefono"}
+                      onChange={this.handleChange}
+                      id={"name"}
                       required={true}
                     />
                   </div>
