cb910051943068fe0e9f082ce3fa34e243e44741
Jonathan M <jmedina2930@gmail.com>
2020-03-29 10:53:23 -0500

    change footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "gatsby";
 
-// import logo from "../img/logo.svg";
+import logo from "../img/logo_small.png";
 // import facebook from "../img/social/facebook.svg";
 // import instagram from "../img/social/instagram.svg";
 // import twitter from "../img/social/twitter.svg";
@@ -11,19 +11,15 @@ const Footer = class extends React.Component {
   render() {
     return (
       <footer className="footer has-background-black has-text-white-ter">
-        {/* <div className="content has-text-centered">
-          <img
-            src={logo}
-            alt="Kaldi"
-            style={{ width: '14em', height: '10em' }}
-          />
-        </div> */}
         <div className="content has-text-centered has-background-black has-text-white-ter">
           <div className="container has-background-black has-text-white-ter">
             <div className="columns">
               <div className="column is-4">
                 <section className="menu">
                   <ul className="menu-list">
+                    <h3 className="has-text-white-ter has-text-left">
+                      Links rápidos
+                    </h3>
                     <li>
                       <Link to="/" className="navbar-item">
                         Inicio
@@ -45,6 +41,9 @@ const Footer = class extends React.Component {
               <div className="column is-4">
                 <section>
                   <ul className="menu-list">
+                    <h3 className="has-text-white-ter has-text-left">
+                      Canales de atención
+                    </h3>
                     <li>
                       <Link className="navbar-item" to="/contactanos">
                         Contáctanos
@@ -54,6 +53,26 @@ const Footer = class extends React.Component {
                       </Link>
                     </li>
                   </ul>
+                </section>
+              </div>
+              <div className="column is-4">
+                <section style={{ paddingTop: "1rem" }}>
+                  <h3 className="has-text-white-ter has-text-left">
+                    Nuestra oficina
+                  </h3>
+                  <div
+                    style={{ paddingLeft: "1rem", paddingTop: "0.5rem" }}
+                    className="has-text-left"
+                  >
+                    <p className="has-text-white-ter">
+                      <strong className="has-text-white-ter">Dirección:</strong>{" "}
+                      Diagonal 50A # 32-200, Bello, Antioquia
+                    </p>
+                    <p className="has-text-white-ter">
+                      <strong className="has-text-white-ter">Teléfono:</strong>{" "}
+                      2983801
+                    </p>
+                  </div>
                 </section>
               </div>
               {/* <div className="column is-4 social">
@@ -87,6 +106,15 @@ const Footer = class extends React.Component {
                   />
                 </a> 
               </div>*/}
+            </div>
+            <hr />
+            <div className="columns">
+              <div className="copyright column is-6 has-text-left">
+                <img src={logo} alt="Orange Consulting" />
+              </div>
+              <div className="copyright column is-6 has-text-right">
+                Copyright Orange Consulting S.A.S. 2020
+              </div>
             </div>
           </div>
         </div>
